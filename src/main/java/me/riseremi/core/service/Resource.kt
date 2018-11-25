31d842dac2c9b349e8ad54cce2f22d050b5b2928
Vitalii Dmitriev <dmitvitalii@gmail.com>
2018-11-25 22:37:54 +0300

    Girl has no name, Resources have one

--- src/main/java/me/riseremi/core/service/Resource.kt
+++ src/main/java/me/riseremi/core/service/Resource.kt
@@ -5,6 +5,7 @@ package me.riseremi.core.service
  * @since 25.11.2018
  */
 data class Resource(
+        val name: String,
         val path: String,
         val type: ResourceManager.ResourceType
 )
