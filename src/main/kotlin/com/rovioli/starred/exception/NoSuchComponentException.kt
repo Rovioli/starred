36b0400ac04977dc3276297e78df0941c9142dd9
Vitalii Dmitriev <dmitvitalii@gmail.com>
2018-11-25 23:22:56 +0300

    Add NoSuchComponentException

---
+++ src/main/kotlin/com/rovioli/starred/exception/NoSuchComponentException.kt
@@ -0,0 +1,7 @@
+package com.rovioli.starred.exception
+
+/**
+ * @author Vitalii Dmitriev
+ * @since 25.11.2018
+ */
+class NoSuchComponentException(override val message: String?) : Exception(message)
